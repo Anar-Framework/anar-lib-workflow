3b48229b0c2572494a9d5e17767ad3d89a9c4abc
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-19 10:22:55 +0430

    workflow data model

--- src/main/java/af/gov/anar/lib/workflow/model/Workflow.java
+++ src/main/java/af/gov/anar/lib/workflow/model/Workflow.java
@@ -10,41 +10,42 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-// @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-// @ToString
-// @EqualsAndHashCode
+@ToString
+@EqualsAndHashCode
 @Entity(name = "Workflow")
 @Table(name = "workflow")
+@Builder
+@DynamicUpdate
 public class Workflow implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "workflow_generator")
-    @SequenceGenerator(name = "workflow_generator", sequenceName = "workflow_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(unique = true, updatable = false, nullable = false)
     private Long id;
 
     @Column(name = "name")
     private String name;
 
+    /**
+     * Specify type of object type that works based on this workflow.
+     */
+    @Column(name = "type")
+    private String type;
+
     @Column(name = "description")
     private String description;
 
-    @Column(name = "env_slug")
-    private String envSlug;
 
     @Column(name = "workflow_json")
     @Type(type = "text")
@@ -54,28 +55,10 @@ public class Workflow implements Serializable {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    public Workflow(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
-    public Workflow(Long id, String name, String description, String envSlug, LocalDateTime createdAt) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.envSlug = envSlug;
-        this.createdAt = createdAt;
-    }
 
-    @Override
-    public String toString() {
-        return "Form [id=" + id + ", name=" + name + ", description=" + description +  ", envSlug=" + envSlug + ", created_at=" + createdAt  + "]";
-    }
 
-    /* Workflow JSON Conditions
-    * authorizedGroups: This is arry type attribute that intoduce restriction on a step based on group.
-                        If this attribute is not defined for any step, that step is visible to everyone means no restriction will be introduced for that step
-                    i.e. "authorizedGroups": ["ADMIN_GROUP", "SUPERVISOR_GROUP"]
-    * if instance current_step is null or empty then first step of workflow should be initial step to be displayed in transition
-    */
 }
