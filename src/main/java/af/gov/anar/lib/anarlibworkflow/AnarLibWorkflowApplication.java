17e790020b944d5ee7bd56763c0197b1b40a8e05
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-19 08:49:37 +0430

    workflow library of anar framework

---
+++ src/main/java/af/gov/anar/lib/anarlibworkflow/AnarLibWorkflowApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.anarlibworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibWorkflowApplication.class, args);
+	}
+
+}
