9257d7316c6844c174da7e8f1f901e630f0e7abe
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-19 10:26:29 +0430

    workflow service

---
+++ src/main/java/af/gov/anar/lib/workflow/service/WorkflowService.java
@@ -0,0 +1,22 @@
+package af.gov.anar.lib.workflow.service;
+
+import af.gov.anar.lib.workflow.model.Workflow;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Component
+@Transactional
+public interface WorkflowService {
+
+    public Workflow createOrUpdate(Workflow workflow);
+    public Workflow update(Workflow workflow, Long id);
+    public List<Workflow> findAll();
+    public List<Workflow> findAllByEnv(String envSlug);
+    public List<Workflow> findAllWithoutJSONContent();
+    public Workflow findById(Long id);
+    public Workflow delete(Long id);
+    public void delete(Workflow workflow);
+
+}
